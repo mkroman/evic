ff8ae1a4aa140a8214502efa6b19f5efcdfd02a8
Mikkel Kroman <mk@uplink.io>
2015-12-17 08:24:45 +0100

    Add application binary and license

--- src/firmware.rs
+++ src/firmware.rs
@@ -1,25 +1,47 @@
-// Copyright (c) 2015 Mikkel Kroman <mk@maero.dk>
+// Copyright (c) 2015, Mikkel Kroman <mk@maero.dk>
 // All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 use std::fs::File;
 use std::io::{Read, Write, Seek, Error, ErrorKind, SeekFrom};
 
 /// The capacity of the ROM in the eVic VTC Mini.
-const EVIC_MINI_ROM_SIZE: usize = 120 * 1024;
+pub const EVIC_MINI_ROM_SIZE: usize = 120 * 1024;
 
 pub struct Firmware {
     buffer: Vec<u8>,
 }
 
 impl Firmware {
-    /// Initialize a new firmware structure with the capacity equal to the ROM capacity.
+    /// Initializes a new firmware structure with the capacity equal to the maximum
+    /// ROM capacity.
     fn new() -> Firmware {
         Firmware {
             buffer: Vec::with_capacity(EVIC_MINI_ROM_SIZE)
         }
     }
 
-    /// Decrypt the firmware behind the provided reader.
+    /// Decrypts the firmware from the provided reader.
     ///
     /// # Examples
     ///
@@ -59,7 +81,7 @@ impl Firmware {
         Ok(firmware)
     }
 
-    /// Write the contents of the firmware buffer to the provided writer.
+    /// Writes the contents of the firmware buffer to the provided writer.
     pub fn save<W: Write + ?Sized>(&self, writer: &mut W) -> Result<(), Error> {
         try!(writer.write_all(&self.buffer[..]));
 
@@ -67,7 +89,7 @@ impl Firmware {
     }
 }
 
-/// Load firmware specified by file path and decrypt it into memory.
+/// Loads a firmware file from the specified path and decrypts it in memory.
 pub fn load(path: &str) -> Result<Firmware, Error> {
     let mut file = try!(File::open(path));
     let firmware = try!(Firmware::decrypt(&mut file));
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,128 @@
-// Copyright (c) 2015 Mikkel Kroman <mk@maero.dk>
+// Copyright (c) 2015, Mikkel Kroman <mk@maero.dk>
 // All rights reserved.
-
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 extern crate evic;
+extern crate getopts;
 
-/// Test.
+use std::fs;
+use std::env;
+use std::path::Path;
+use std::error::Error;
+use getopts::Options;
+use evic::firmware;
+
+const NAME: &'static str = "evicutil";
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn print_usage(program: &str, options: &Options) {
+    let brief = format!("Usage: {} [OPTIONS] COMMAND [arg...]", program);
+
+    print!("{}", options.usage(&brief));
+}
+
+fn print_version() {
+    println!("{} {}", NAME, VERSION);
+}
+
+fn validate_input_file<'a, P: AsRef<Path>>(path: P) -> Result<(), &'static str> {
+    let path = path.as_ref();
+
+    if path.exists() && path.is_file() {
+        return Err("The specified path does not exist");
+    }
+
+    let metadata = match path.metadata() {
+        Ok(metadata) => metadata,
+        Err(error) => return Err(&error.to_string())
+    };
+
+    let size = metadata.len();
+
+    if size > 0 && size < firmware::EVIC_MINI_ROM_SIZE as u64 {
+        Ok(())
+    } else {
+        Err("The input file doesn't fit the criteria of a firmware file")
+    }
+}
+
+fn encrypt(args: &Vec<String>, options: &Options) {
+    let filename = args.get(1);
+    
+}
+
+fn decrypt(args: &Vec<String>, options: &Options) {
+   let filename = match args.get(1) {
+        Some(filename) => filename,
+        None => {
+            print_usage(NAME, options);
+            return;
+        }
+    };
+
+   match validate_input_file(&filename) {
+       Ok(()) => {
+           println!("Decrypting {}", filename);
+       },
+       Err(error) => {
+           println!("{}", error);
+       }
+   }
+}
+
 fn main() {
-    println!("hi");
+    let args: Vec<String> = env::args().collect();
+    let program = &args[0];
+
+    let mut options = Options::new();
+    options.optopt("o", "", "set output filename", "NAME");
+    options.optflag("h", "help", "print this help menu");
+    options.optflag("v", "version", "output version information and exit");
+
+    let matches = match options.parse(&args[1..]) {
+        Ok(matches) => matches,
+        Err(error) => panic!(error.to_string())
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, &options);
+        return;
+    }
+
+    if matches.opt_present("v") {
+        print_version();
+        return; 
+    }
+
+    match matches.free.get(0) {
+        Some(x) if x == "encrypt" => encrypt(&matches.free, &options),
+        Some(x) if x == "decrypt" => decrypt(&matches.free, &options),
+        Some(x) => {
+            println!("Unknown command `{}'", x);
+        },
+        None => {
+            print_usage(&program, &options);
+        }
+    }
+
+    return;
 }
